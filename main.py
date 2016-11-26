14aee4fc900929a87b8b49b5649b11d574227e7c
mazenbesher <mazenbesher@gmail.com>
2016-11-26 17:59:09 +0100

    2 Phase Simplex with new Tests

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
+# TODO: one simple main loop function (as private method __)
+# TODO: convert to standard form if not in it
 
 import numpy as np
 import copy
 from numpy.linalg import inv
 
-LIMIT = 1000 # maximale Anzahl der Durchläufe des Simplex-Algorithmus
+LIMIT = 1000  # maximale Anzahl der Durchläufe des Simplex-Algorithmus
 
 # Hilfsfunktionen 
 rows = lambda X: X.shape[0]  # Anzahl der Zeile einer Matrix
 cols = lambda X: X.shape[1]  # Anzahl der Spalten einer Matrix
-base_zero = lambda X: [i - 1 for i in X]  # Wandel eine Liste in Basis 1 um (für die LP-Basis)
 
 
 # Eine Klasse um ein LP-Problem zu repräsentieren
 class LP:
-    def __init__(self, A, b, c, B):
+    def __init__(self, A, b, c):
+        """
+        A class to represent a standard LP (max cTx s.t. A.x <= b and x >= 0)
+        """
         self.A = A
         self.b = b
         self.c = c
-        self.B = B  # zur Basis 1 (nicht 0)
 
 
 # Hilfsfunktion um die durch index definierten Spalten aus A zurückzugeben
@@ -29,24 +32,224 @@ def indexed(X, index):
     return res
 
 
+# Hilfsfunktion, sucht nach einem zulaessigen Startbasisloesung falls
+# das LP keine hat (d.h. NULL is nicht zuelassig am Anfang <=> not b >= 0)
+def first_phase(lp):
+    """
+    :return: a feasibal start basis if exists or None if none exists
+    """
+    # das Hilfsproblem formulieren
+    new_col = np.transpose(np.matrix(np.zeros(rows(lp.A)) - 1))
+    A = np.concatenate((new_col, lp.A), axis=1)
+    b = lp.b
+    c = np.vstack([np.matrix('-1'), np.transpose(np.matrix(np.zeros(cols(A) - 1)))])
+
+    # Pivotisierung
+    # Variablen definieren
+    I = np.matrix(np.identity(rows(A)))
+    AI = np.concatenate((A, I), 1)
+    x = np.matrix(np.zeros(cols(AI)))
+    c = np.concatenate((c, np.transpose(np.matrix(np.zeros(rows(b))))), axis=0)  # extend c for the slack variables
+    c = np.matrix(np.transpose(c))  # use the transpose just to make operations easier
+
+    # the origin is now feasible basis in the hilfsproblem
+    B = [cols(A) + i for i in range(rows(A))]
+
+    # eintretende x_0
+    j = 0
+
+    # veralssende x_i mit max {|b_i|: b_i <0}
+    i = max([(abs(i), abs(int(k))) for i, k in enumerate(b) if int(k) < 0],
+            key=lambda x: x[1])[0]
+
+    # Update Basis
+    B[i] = j
+    B.sort()
+
+    # solve using simplex since now the basis is feasible
+    # 0. Init
+    A_B = indexed(AI, B)
+    N = [i for i in range(rows(A) + cols(A)) if not i in B]
+    x_B = np.dot(inv(A_B), b)
+
+    # Update x
+    B_pointer = 0
+    for i in range(cols(AI)):
+        if i in B:
+            x[0, i] = x_B[B_pointer, 0]
+            B_pointer += 1
+
+    # Main-Loop
+    for counter in range(LIMIT):
+        # 1. BTRAN
+        A_B = indexed(AI, B)
+        c_B = indexed(c, B)
+        ys = np.matrix(
+            np.transpose(
+                np.dot(c_B, inv(A_B))
+            )
+        )
+
+        # 2. Pricing
+        c_N = indexed(c, N)
+        A_N = indexed(AI, N)
+        cs_N = np.subtract(np.transpose(c_N), np.matrix(np.dot(np.transpose(A_N), ys)))
+        if np.all(cs_N <= 0):
+            # Optimale Lösung gefunden
+            # Ueberpruefen ob der Zielfunktionswert = 0
+            if np.dot(c, np.transpose(x)).item(0) != 0:
+                return None
+            else:
+                # generate new problem (remove x_0 col and reformat z)
+                # and solve it with simplex
+                N.remove(0)
+                x = x[:,1:]
+                AI = AI[:,1:]
+                # convert B, N to zero base
+                N = [i - 1 for i in N]
+                B = [i - 1 for i in B]
+                return second_phase(B, N, AI, x, lp.c, b)
+        else:
+            for i in range(len(cs_N)):
+                if np.all(cs_N[i] > 0):
+                    j = N[i]
+                    break
+
+        # 3. FTRAN
+        A_j = indexed(AI, [j])
+        w = np.matrix(np.dot(
+            inv(A_B), A_j
+        ))
+
+        # 4. Ratio-Test
+        if np.all(w <= 0):
+            return None  # unbounded / unbeschränkt
+        else:
+            arr = []
+            for i in range(len(B)):
+                k = B[i]
+                x_k = x[0, k]
+                w_k = w[i, 0]
+                if (w_k > 0): arr.append(((x_k / w_k), k))
+            # Taktik (immer x_0 die Basis zu verlassen wenn moeglich)
+            for t_cand, i_cand in arr:
+                if i_cand == 0 and t_cand == min(arr)[0]:
+                    t_star, i = t_cand, i_cand
+                    break
+            else: # else if the for-loop didn't break (Python only :)
+                # x_0 is not candidate
+                t_star, i = min(arr)
+
+        # 5. Update
+        x_B = np.transpose(indexed(x, B))
+        B_pointer = 0
+        for l in range(cols(x)):
+            if l in B:
+                x[0, l] = x_B[B_pointer, 0] - (t_star * w[B_pointer, 0])
+                B_pointer += 1
+
+        x[0, j] = t_star
+
+        B.remove(i)
+        B.append(j)
+        B.sort()
+
+        N.remove(j)
+        N.append(i)
+        N.sort()
+
+# Hilfsfunktion, simplex nach der 1. Phase
+def second_phase(B, N, AI, x, c, b):
+    c = np.concatenate((c, np.transpose(np.matrix(np.zeros(rows(b))))), axis=0)  # extend c for the slack variables
+    c = np.matrix(np.transpose(c))  # use the transpose just to make operations easier
+
+    # Main-Loop
+    for counter in range(LIMIT):
+        # 1. BTRAN
+        A_B = indexed(AI, B)
+        c_B = indexed(c, B)
+        ys = np.matrix(
+            np.transpose(
+                np.dot(c_B, inv(A_B))
+            )
+        )
+
+        # 2. Pricing
+        c_N = indexed(c, N)
+        A_N = indexed(AI, N)
+        cs_N = np.subtract(np.transpose(c_N), np.matrix(np.dot(np.transpose(A_N), ys)))
+        if np.all(cs_N <= 0):
+            return B, x  # Optimale Lösung gefunden
+        else:
+            for i in range(len(cs_N)):
+                if np.all(cs_N[i] > 0):
+                    j = N[i]
+                    break
+
+        # 3. FTRAN
+        A_j = indexed(AI, [j])
+        w = np.matrix(np.dot(
+            inv(A_B), A_j
+        ))
+
+        # 4. Ratio-Test
+        if np.all(w <= 0):
+            return None # unbeschränkt / unbounded
+        else:
+            arr = []
+            for i in range(len(B)):
+                k = B[i]
+                x_k = x[0, k]
+                w_k = w[i, 0]
+                if (w_k > 0): arr.append(((x_k / w_k), k))
+            t_star, i = min(arr)
+
+        # 5. Update
+        x_B = np.transpose(indexed(x, B))
+        B_pointer = 0
+        for l in range(cols(x)):
+            if l in B:
+                x[0, l] = x_B[B_pointer, 0] - (t_star * w[B_pointer, 0])
+                B_pointer += 1
+
+        x[0, j] = t_star
+
+        B.remove(i)
+        B.append(j)
+        B.sort()
+
+        N.remove(j)
+        N.append(i)
+        N.sort()
+
+        counter += 1
+
 # Hauptfunktion
 def simplex(lp, debug=False):
     """
-    No cycling (kreise) will happen because of the selection of the sorting of
-    the index sets (selecting the candidate with the smallest index - Bland's Rule)
+    No cycling (kreisen) will happen because of the sorting of the index
+     sets (selecting the candidate with the smallest index - Bland's Rule)
     Example see test_simplexKreisen in test.py
 
-    :param lp: LP linear program
+    :param lp: LP linear program (see LP class)
     :param debug: debug mode (see last condition in this function)
     :return:
     """
     # Variablen definieren
-    A, b, c, B = lp.A, lp.b, lp.c, lp.B
-    B = base_zero(B)
+    A, b, c = lp.A, lp.b, lp.c
     I = np.matrix(np.identity(rows(A)))
     AI = np.concatenate((A, I), 1)
     x = np.matrix(np.zeros(cols(AI)))
-    c = np.matrix(np.transpose(c))  # just to make operations easier
+    c = np.concatenate((c, np.transpose(np.matrix(np.zeros(rows(b))))), axis=0)  # extend c for the slack variables
+    c = np.matrix(np.transpose(c))  # use the transpose just to make operations easier
+
+    # Search for feasible basis
+    if np.all(b >= 0):
+        # the origin is feasible basis
+        B = [cols(A) + i for i in range(rows(A))]
+    else:
+        # we need to search for a feasible basis
+        return first_phase(lp)
 
     # 0. Init
     A_B = indexed(AI, B)
@@ -76,7 +279,7 @@ def simplex(lp, debug=False):
         A_N = indexed(AI, N)
         cs_N = np.subtract(np.transpose(c_N), np.matrix(np.dot(np.transpose(A_N), ys)))
         if np.all(cs_N <= 0):
-            return B, x # Optimale Lösung gefunden
+            return B, x  # Optimale Lösung gefunden
         else:
             for i in range(len(cs_N)):
                 if np.all(cs_N[i] > 0):
@@ -91,7 +294,7 @@ def simplex(lp, debug=False):
 
         # 4. Ratio-Test
         if np.all(w <= 0):
-            return "unbounded" # unbeschränkt
+            return "unbounded"  # unbeschränkt
         else:
             arr = []
             for i in range(len(B)):
@@ -99,7 +302,6 @@ def simplex(lp, debug=False):
                 x_k = x[0, k]
                 w_k = w[i, 0]
                 if (w_k > 0): arr.append(((x_k / w_k), k))
-
             t_star, i = min(arr)
 
         # 5. Update
--- test.py
+++ test.py
@@ -5,57 +5,91 @@ import numpy as np
 
 from main import *
 
-aufgabe1 = LP( # Blatt 2
+blatt2_aufgabe1 = LP(
     np.matrix('2 0 6; -2 8 4; 3 6 5'),
     np.matrix('10; 12; 20'),
-    np.matrix('2; 1; 3; 0; 0; 0'),
-    [4, 5, 6])
-
-vorlesung = LP( # Muesli Example
-    np.matrix('2 3; 4 1; 1 1'),
-    np.matrix('12000; 16000; 4300'),
-    np.matrix('5; 4; 0; 0; 0'),
-    [3, 4, 5])
-
-kreise_example = LP( # Book Page 31
-    np.matrix('-0.5 -5.5 -2.5 9; 0.5 -1.5 -0.5 1; 1 0 0 0'),  # A
-    np.matrix('0; 0; 1'),  # b
-    np.matrix('10; -57; -9; -24; 0; 0; 0'),  # c
-    [5, 6, 7]
+    np.matrix('2; 1; 3'),
 )
 
+vorlesung = LP(  # Muesli Example
+    np.matrix('2 3; 4 1; 1 1'),
+    np.matrix('12000; 16000; 4300'),
+    np.matrix('5; 4'),
+)
 
+kreise_example = LP(  # Book Page 31
+    np.matrix('-0.5 -5.5 -2.5 9; 0.5 -1.5 -0.5 1; 1 0 0 0'),  # A
+    np.matrix('0; 0; 1'),  # b
+    np.matrix('10; -57; -9; -24'),  # c
+)
+
+blatt5_aufgabe1 = LP(  # extended muesli example
+    np.matrix('2 3; 4 1; 1 1; 2 1'),  # A
+    np.matrix('12000; 16000; 4300; 8200'),  # b
+    np.matrix('5; 4'),  # c
+)
+
+infeasible_start = LP(  # Blatt4 Aufgabe2
+    np.matrix('-2 -1; -1 -2; -1 -1; 3 4; 1 0'),
+    np.matrix('-12; -12; -10; 60; 12'),
+    np.matrix('11; 8')
+)
+
+unbounden = LP(  # see https://www.desmos.com/calculator/7g2vcxlt7p
+    np.matrix('-1 -1; 1 1'),
+    np.matrix('-2; 1'),
+    np.matrix('1; 1')
+)
 
 
 class TestIndexed(unittest.TestCase):
     def test_indexedOneColumn(self):
         self.assertTrue(np.array_equal(
-            indexed(aufgabe1.A, [1]),
+            indexed(blatt2_aufgabe1.A, [1]),
             np.matrix('0; 8; 6')))
 
     def test_indexedTwoColumn(self):
         self.assertTrue(np.array_equal(
-            indexed(aufgabe1.A, [0, 2]),
+            indexed(blatt2_aufgabe1.A, [0, 2]),
             np.matrix('2 6; -2 4; 3 5')))
 
 
-class TestSimplex(unittest.TestCase):
+class TestSimplexSpecialCases(unittest.TestCase):
+    def test_simplexKreisen(self):
+        solu = simplex(kreise_example)[1]
+        self.assertTrue(np.all(solu[0, 0:4] == [1, 0, 1, 0]))
+
+    def test_simplexInfeasibleStart(self):
+        # requires 2.Phase Simplex
+        solu = simplex(infeasible_start)[1]
+        x_1, x_2 = solu[0, 0], solu[0, 1]
+        self.assertEqual(x_1, 12)
+        self.assertEqual(x_2, 6)
+
+    def test_simplexUnbounden(self):
+        # requires 2.Phase Simplex
+        self.assertIsNone(simplex(unbounden))
+
+
+class TestSimplexRandomExamples(unittest.TestCase):
     def test_simplexVorlesung(self):
         solu = simplex(vorlesung)[1]
         x_1, x_2 = int(solu[0, 0]), int(solu[0, 1])
         self.assertEqual(x_1, 3900)
         self.assertEqual(x_2, 400)
 
-    def test_simplexAufgabe1(self):
-        solu = simplex(aufgabe1)[1]
+    def test_simplexBlatt2Aufgabe1(self):
+        solu = simplex(blatt2_aufgabe1)[1]
         x_1, x_2, x_3 = int(solu[0, 0]), solu[0, 1], int(solu[0, 2])
         self.assertEqual(x_1, 5)
         self.assertAlmostEqual(x_2, 0.83333333)
         self.assertEqual(x_3, 0)
-        
-    def test_simplexKreisen(self):
-        solu = simplex(kreise_example)[1]
-        self.assertTrue(np.all(solu[0,0:4] == [1, 0, 1, 0]))
+
+    def test_simplexBlatt5Aufgabe1(self):
+        solu = simplex(blatt5_aufgabe1)[1]
+        x_1, x_2 = int(solu[0, 0]), int(solu[0, 1])
+        self.assertEqual(x_1, 3900)
+        self.assertEqual(x_2, 400)
 
 
 if __name__ == '__main__':
